61625acb9ad606222d5a104d8533ed9fb768cf6c
root <root@setorsete.localdomain>
2018-08-10 01:40:42 +0000

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mvcdotnetcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
